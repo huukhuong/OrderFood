b94c85b634e883a0cff0c9e6f8eba17085d36d44
huukhuong <huukhuong.it@gmail.com>
2021-12-11 21:16:13 +0700

    update database seeder

--- database/seeders/Products.php
+++ database/seeders/Products.php
@@ -14,76 +14,130 @@ class Products extends Seeder
      */
     public function run()
     {
-        DB::table('products')-> insert([
-            [   'name' => 'Pizza 1',
+        DB::table('products')->insert([
+            [
+                'name' => 'Pizza 1',
                 'description' => 'Pizza rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'pizza0.png',
                 'category_id' => '1',
                 'status' => '1'
             ],
-            [   'name' => 'Pizza 2',
+            [
+                'name' => 'Pizza 2',
                 'description' => 'Pizza2 rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'pizza1.png',
                 'category_id' => '1',
                 'status' => '1'
             ],
-            [   'name' => 'Pizza 3',
+            [
+                'name' => 'Pizza 3',
                 'description' => 'Pizza3 rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'pizza2.png',
                 'category_id' => '1',
                 'status' => '1'
             ],
-            [   'name' => 'Hamberger 1',
-                'description' => 'Hamberger rất ngon',
+            [
+                'name' => 'Pizza 3',
+                'description' => 'Pizza3 rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'pizza3.png',
+                'category_id' => '1',
+                'status' => '1'
+            ],
+            [
+                'name' => 'Pizza 3',
+                'description' => 'Pizza3 rất ngon',
+                'quantity' => '100',
+                'price' => '10000',
+                'image' => 'pizza4.png',
+                'category_id' => '1',
+                'status' => '1'
+            ],
+            [
+                'name' => 'Pizza 3',
+                'description' => 'Pizza3 rất ngon',
+                'quantity' => '100',
+                'price' => '10000',
+                'image' => 'pizza5.png',
+                'category_id' => '1',
+                'status' => '1'
+            ],
+            [
+                'name' => 'Pizza 3',
+                'description' => 'Pizza3 rất ngon',
+                'quantity' => '100',
+                'price' => '10000',
+                'image' => 'pizza6.png',
+                'category_id' => '1',
+                'status' => '1'
+            ],
+            [
+                'name' => 'Pizza 3',
+                'description' => 'Pizza3 rất ngon',
+                'quantity' => '100',
+                'price' => '10000',
+                'image' => 'pizza7.png',
+                'category_id' => '1',
+                'status' => '1'
+            ],
+            [
+                'name' => 'Hamburger 1',
+                'description' => 'Hamburger rất ngon',
+                'quantity' => '100',
+                'price' => '10000',
+                'image' => 'hamburger0.jpg',
                 'category_id' => '2',
                 'status' => '1'
             ],
-            [   'name' => 'Hamberger 2',
-                'description' => 'Hamberger2 rất ngon',
+            [
+                'name' => 'Hamburger 2',
+                'description' => 'Hamburger2 rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'hamburger1.jpg',
                 'category_id' => '2',
                 'status' => '1'
             ],
-            [   'name' => 'Hamberger 3',
-                'description' => 'Hamberger3 rất ngon',
+            [
+                'name' => 'Hamburger 3',
+                'description' => 'Hamburger3 rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'hamburger2.jpg',
                 'category_id' => '2',
                 'status' => '1'
             ],
-            [   'name' => 'Gà Rán 1',
+            [
+                'name' => 'Gà Rán 1',
                 'description' => 'Gà Rán rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'ga-ran1.jpg',
                 'category_id' => '3',
                 'status' => '1'
             ],
-            [   'name' => 'Gà Rán 2',
+            [
+                'name' => 'Gà Rán 2',
                 'description' => 'Gà Rán2 rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'ga-ran2.jpg',
                 'category_id' => '3',
                 'status' => '1'
             ],
-            [   'name' => 'Gà Rán 3',
+            [
+                'name' => 'Gà Rán 3',
                 'description' => 'Gà Rán rất ngon',
                 'quantity' => '100',
                 'price' => '10000',
-                'image' => 'meow.jpg',
+                'image' => 'ga-ran3.jpg',
                 'category_id' => '3',
                 'status' => '1'
             ]
